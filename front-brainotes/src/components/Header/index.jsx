0992fa3d5db5b5f437dbeee6b99eb0e2e46b9009
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-22 09:12:37 -0300

    Instal react icons & Adding logout profile icon

--- front-brainotes/src/components/Header/index.jsx
+++ front-brainotes/src/components/Header/index.jsx
@@ -1,4 +1,5 @@
-import { Container, Profile } from "./styles";
+import {RiShutDownLine} from 'react-icons/ri'
+import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
 
@@ -11,6 +12,9 @@ export function Header() {
           <strong>Caio Balduino</strong>
         </div>
       </Profile>
+      <Logout>
+        <RiShutDownLine/>
+      </Logout>
     </Container>
   )
 }
