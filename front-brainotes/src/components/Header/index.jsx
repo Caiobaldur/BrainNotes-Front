cb91f55d15b365ec21171a6c10c60348bcf4c081
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-21 22:56:18 -0300

    Inclusion of the user in the page header

--- front-brainotes/src/components/Header/index.jsx
+++ front-brainotes/src/components/Header/index.jsx
@@ -1,10 +1,16 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
 
   return(
     <Container>
-      
+      <Profile>
+        <img src="https://github.com/caiobaldur.png" alt="Foto do usuário" />
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Caio Balduino</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
