c838b6691c5d8bcb9171591b9291f7974abfeef3
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-22 12:26:01 -0300

    Details interface finalization

--- front-brainotes/src/pages/Details/Index.jsx
+++ front-brainotes/src/pages/Details/Index.jsx
@@ -1,4 +1,4 @@
-import {Container, Links} from './styles';
+import {Container, Links, Content} from './styles';
 
 import { Tag } from '../../components/Tag';
 import { Header } from '../../components/Header';
@@ -12,23 +12,35 @@ export function Details () {
   return(
     <Container>
       <Header/>
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota"/>
 
-      <ButtonText title="Excluir nota"/>
+            <h1>
+              Introdução ao React
+            </h1>
 
-      <Section title="Links Úteis">
-        <Links>
-          <li><a href="#">Link 1</a></li>
-          <li><a href="#">Link 2</a></li>
-          <li><a href="#">Link 3</a></li>
-        </Links>
-      </Section>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi suscipit sed nihil nostrum id architecto, 
+              dolorem voluptatem amet totam, ea debitis enim mollitia voluptate. Odio quibusdam saepe sint consectetur vitae?
+            </p>
 
-      <Section title="Marcadores">
-        <Tag title="Express"/>
-        <Tag title="Node"/>
-      </Section>
+              <Section title="Links Úteis">
+                <Links>
+                  <li><a href="#">Link 1</a></li>
+                  <li><a href="#">Link 2</a></li>
+                  <li><a href="#">Link 3</a></li>
+                </Links>
+              </Section>
 
-      <Button title="Voltar" />
+              <Section title="Marcadores">
+                <Tag title="Express"/>
+                <Tag title="Node"/>
+              </Section>
+
+            <Button title="Voltar" />
+         </Content>
+        </main>
     </Container>
   )
 }
