210cc106656c07e1dcd367b3e570c80cf5f4ef0f
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-26 18:09:04 -0300

    Profile User stylization

--- front-brainotes/src/pages/Profile/index.jsx
+++ front-brainotes/src/pages/Profile/index.jsx
@@ -1,7 +1,7 @@
-import { FiArrowLeft, FiUser, FiMail , FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiMail , FiLock, FiCamera } from 'react-icons/fi'
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
 
@@ -13,7 +13,13 @@ export function Profile() {
         </a>
       </header>
       <Form>
-        
+        <Avatar>
+          <img src="https://github.com/Caiobaldur.png" alt="Foto do Usuário" />
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input id='avatar' type='file'/>
+          </label>
+        </Avatar>
         <Input 
           placeholder="Nome"
           type="text"
