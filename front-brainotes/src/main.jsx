cd0697d0366ff90ad9e6677bfbb71f7f4c669542
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-25 22:17:39 -0300

    Creating the sign in page

--- front-brainotes/src/main.jsx
+++ front-brainotes/src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Home} from './pages/Home/index.jsx'
+// import {Home} from './pages/Home/index.jsx'
 import {ThemeProvider} from 'styled-components'
 import theme from './styles/theme.js'
 import GlobalStyles from './styles/global.js'
+import {SignIn} from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
