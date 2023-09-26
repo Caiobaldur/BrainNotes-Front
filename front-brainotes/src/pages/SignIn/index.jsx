ac9da972863da06fecd7e0e504724658bd027a06
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-25 22:41:35 -0300

    Styling the Sign In (form)

--- front-brainotes/src/pages/SignIn/index.jsx
+++ front-brainotes/src/pages/SignIn/index.jsx
@@ -1,10 +1,37 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
 
 export function SignIn() {
 
   return (
     <Container>
+      <Form>
+        <h1>Brain Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis!</p>
 
+        <h2>Faça seu Login</h2>
+
+        <Input 
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input 
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Entrar" />
+
+        <a href="#">
+          Criar Conta
+        </a>
+      </Form>
     </Container>
   );
 };
