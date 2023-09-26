a75776be5142b02ed90a7ee354b5d833c34df037
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-26 17:52:04 -0300

    Profile component Creation

--- front-brainotes/src/main.jsx
+++ front-brainotes/src/main.jsx
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom/client'
 import {ThemeProvider} from 'styled-components'
 import theme from './styles/theme.js'
 import GlobalStyles from './styles/global.js'
-import {SignIn} from './pages/SignIn'
-import {SignUp} from './pages/SignUp'
+import {Profile} from './pages/Profile'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
