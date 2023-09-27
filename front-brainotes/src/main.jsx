11d092928a95f7d65167c7f2d59235377be81cf2
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-27 14:02:18 -0300

    Finishing the front-end (navigation and routes)

--- front-brainotes/src/main.jsx
+++ front-brainotes/src/main.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import {Home} from './pages/Home/index.jsx'
-import {ThemeProvider} from 'styled-components'
-import theme from './styles/theme.js'
 import GlobalStyles from './styles/global.js'
-import {New} from './pages/New'
+import {ThemeProvider} from 'styled-components'
+
+import theme from './styles/theme.js'
+
+import {Routes} from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
