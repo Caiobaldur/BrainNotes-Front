5f3cf38b2d9178a97982bacd3742f4a708219bfd
Caio Balduino <caiobrafachino@hotmail.com>
2023-11-13 22:32:38 -0300

    remover links

--- front-brainotes/src/pages/New/index.jsx
+++ front-brainotes/src/pages/New/index.jsx
@@ -18,6 +18,10 @@ export function New() {
     setNewLink("");
   }
 
+  function handleRemoveLink(deleted){
+    setLinks(prevState => prevState.filter(link => link !== deleted));
+  }
+
 
   return(
     <Container>
@@ -36,7 +40,7 @@ export function New() {
           <Section title="Links úteis">
             {
               links.map((link, index) => (
-                <NoteItem key={String(index)} value={link} onClick={() => {}}/>
+                <NoteItem key={String(index)} value={link} onClick={() => handleRemoveLink(link)}/>
               ))
             }
             <NoteItem isNew placeholder="Novo Link" value={newLink} onChange={e => setNewLink(e.target.value)} onClick={handleAddLink}/>
