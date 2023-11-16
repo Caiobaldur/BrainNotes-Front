88f66d59deee7ba4e13cd810569d6e9d9499de22
Caio Balduino <caiobrafachino@hotmail.com>
2023-11-15 23:35:38 -0300

    adicionando tags

--- front-brainotes/src/pages/New/index.jsx
+++ front-brainotes/src/pages/New/index.jsx
@@ -13,6 +13,9 @@ export function New() {
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState("");
 
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
   function handleAddLink(){
     setLinks(prevState => [...prevState, newLink]);
     setNewLink("");
@@ -20,6 +23,11 @@ export function New() {
 
   function handleRemoveLink(deleted){
     setLinks(prevState => prevState.filter(link => link !== deleted));
+  }
+
+  function handleAddTag(){
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag("");
   }
 
 
@@ -48,8 +56,12 @@ export function New() {
 
           <Section title="Marcadores">
             <div className='tags'>
-            <NoteItem value="React"/>
-            <NoteItem isNew placeholder="Nova Tag"/>
+              {
+                tags.map((tag, index) => (
+                  <NoteItem key={String(index)} value={tag} onClick={() => {}}/>
+                  ))
+              }
+            <NoteItem isNew placeholder="Nova Tag" onChange={e=> setNewTag(e.target.value)} value={newTag} onClick={handleAddTag} />
             </div>
           </Section>
 
